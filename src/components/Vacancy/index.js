2ba5c6aa2c9daefcaffb23cf1bd9096a7ddd3852
remiixsp <Battlenet22>
2022-10-31 21:33:30 +0200

    adding vacancy info page

--- src/components/Vacancy/index.js
+++ src/components/Vacancy/index.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const vacancies = [
+  {
+    id: 0,
+    name: 'Прибиральник',
+    salary: 8000,
+    description: 'Треба прибирати',
+  },
+  { id: 1, name: 'Ойтішник', salary: 12000, description: 'Треба кодити' },
+  {
+    id: 2,
+    name: 'Бухгалтер',
+    salary: 18000,
+    description: 'Треба рахувати гроші',
+  },
+];
 
 const Vacancy = () => {
-  return <div>Vacancy</div>;
+  const { id } = useParams();
+  const vacancy = vacancies.find((obj) => obj.id === Number(id));
+
+  return <div>{vacancy.name}</div>;
 };
 
 export default Vacancy;
