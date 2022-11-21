5d60922d0ce16fc7122aa704b64850c3f52c82e9
remiixsp <Battlenet22>
2022-11-22 00:15:41 +0200

    design fix

--- src/components/VacancysList/vacancy/index.js
+++ src/components/VacancysList/vacancy/index.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import "./styles.scss";
-import Api, { baseURL } from "../../../api";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './styles.scss';
+import Api, { baseURL } from '../../../api';
 
 const Vacancy = ({ id, name, description, salary, user, status, update }) => {
   return (
-    <div className="each-vacancy__full">
-      <div className="each-vacancy">
-        <NavLink to={`/vacancies/${id}`}>
-          <p className="each-vacancy__name">{name}</p>
+    <div className='each-vacancy__full'>
+      <div className='each-vacancy'>
+        <NavLink className='info__link' to={`/vacancies/${id}`}>
+          <p className='each-vacancy__name'>{name}</p>
         </NavLink>
-        <div className="description-block">
-          <p className="description-block__description">Опис: {description}</p>
-          <p className="description-block__salary">Зарплата: {salary}</p>
+        <div className='description-block'>
+          <p className='description-block__description'>Опис: {description}</p>
+          <p className='description-block__salary'>Зарплата: {salary}</p>
         </div>
 
         {user.isAdmin && (
           <>
             {!status && (
               <button
-                className="each-vacancy__participate"
+                className='each-vacancy__participate'
                 onClick={(e) => {
                   Api.put(`vacancy/approve/${id}`).then(() => {
                     update();
@@ -32,7 +32,7 @@ const Vacancy = ({ id, name, description, salary, user, status, update }) => {
             )}
             {status && (
               <button
-                className="each-vacancy__participate"
+                className='each-vacancy__participate'
                 onClick={(e) => {
                   Api.put(`vacancy/decline/${id}`).then(() => {
                     update();
