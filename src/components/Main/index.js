320b1e5ecf8c2eecf18bebb626d371fe3744fc32
remiixsp <Battlenet22>
2022-11-26 01:37:17 +0200

    .

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,11 +1,15 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './style.scss';
 
 const Main = () => {
   return (
-    <div>
-      Main
-      <NavLink to="/create-vacancy">Створити вакансію</NavLink>
+    <div className='container'>
+      <button className='create-vac'>
+        <NavLink className='create-vac__link' to='/create-vacancy'>
+          Створити вакансію
+        </NavLink>
+      </button>
     </div>
   );
 };
