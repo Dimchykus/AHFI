177499e773ade1090be885a5dc8ac65cd81e3ece
remiixsp <Battlenet22>
2022-11-28 00:23:46 +0200

    design

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,21 +1,21 @@
-import { useEffect, useState } from "react";
-import Api, { sessionSet, toast } from "../../api";
-import { useNavigate } from "react-router-dom";
-import "./style.scss";
+import { useEffect, useState } from 'react';
+import Api, { sessionSet, toast } from '../../api';
+import { useNavigate } from 'react-router-dom';
+import './style.scss';
 
 const Login = ({ setUser, user }) => {
   let navigate = useNavigate();
   const [state, setState] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPass] = useState("");
-  const [name, setName] = useState("");
-  const [vik, setVik] = useState("");
-  const [dosvid, setDosvid] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPass] = useState('');
+  const [name, setName] = useState('');
+  const [vik, setVik] = useState('');
+  const [dosvid, setDosvid] = useState('');
 
   const submit = () => {
     if (!state) {
       if (!password) {
-        toast("Введіть пароль", "error");
+        toast('Введіть пароль', 'error');
         return;
       }
 
@@ -23,139 +23,143 @@ const Login = ({ setUser, user }) => {
         .then((res) => {
           console.log(res);
           setUser(res.data);
-          navigate("/");
+          navigate('/');
 
-          sessionSet("user", res.data);
+          sessionSet('user', res.data);
         })
         .catch(() => {
-          toast("Неправильні дані, перевірте пароль та пошту!!!", "error");
+          toast('Неправильні дані, перевірте пароль та пошту!!!', 'error');
         });
     } else {
-      if (!email) toast("Введіть пошту");
-      if (!name) toast("Введіть імя");
-      if (!vik) toast("Введіть вік");
-      if (!dosvid) toast("Введіть досвід");
+      if (!email) toast('Введіть пошту');
+      if (!name) toast('Введіть імя');
+      if (!vik) toast('Введіть вік');
+      if (!dosvid) toast('Введіть досвід');
 
       if (!email || !name || !vik || !dosvid) {
         return;
       }
 
-      Api.post("users", {
+      Api.post('users', {
         email,
         name,
         age: vik,
         experience: dosvid,
         isAdmin: false,
-        userKeys: "java",
+        userKeys: 'java',
       }).then((res) => {
         setUser(res.data._embedded);
-        navigate("/");
+        navigate('/');
       });
     }
   };
 
   useEffect(() => {
-    if (user) navigate("/");
+    if (user) navigate('/');
   }, [user]);
 
   return state ? (
-    <div>
-      <div class="login_inputBlock">
-        <p class="login_inputTitle">Пошта</p>
+    <div className='registration-cont'>
+      <div class='login_inputBlock'>
+        <p class='login_inputTitle'>Пошта</p>
         <input
-          type="text"
+          type='text'
           value={email}
-          id="city"
-          className="login_input"
+          id='city'
+          className='login_input'
           onChange={(e) => {
             setEmail(e.target.value);
           }}
         />
       </div>
-      <div class="login_inputBlock">
-        <p class="login_inputTitle">Імя</p>
+      <div class='login_inputBlock'>
+        <p class='login_inputTitle'>Імя</p>
         <input
-          type="text"
+          type='text'
           value={name}
-          id="city"
-          className="login_input"
+          id='city'
+          className='login_input'
           onChange={(e) => {
             setName(e.target.value);
           }}
         />
       </div>
-      <div class="login_inputBlock">
-        <p class="login_inputTitle">Вік</p>
+      <div class='login_inputBlock'>
+        <p class='login_inputTitle'>Вік</p>
         <input
-          type="text"
+          type='text'
           value={vik}
-          id="city"
-          className="login_input"
+          id='city'
+          className='login_input'
           onChange={(e) => {
             setVik(e.target.value);
           }}
         />
       </div>
-      <div class="login_inputBlock">
-        <p class="login_inputTitle">Досвід</p>
+      <div class='login_inputBlock'>
+        <p class='login_inputTitle'>Досвід</p>
         <input
-          type="text"
+          type='text'
           value={dosvid}
-          id="city"
-          className="login_input"
+          id='city'
+          className='login_input'
           onChange={(e) => {
             setDosvid(e.target.value);
           }}
         />
       </div>
-      <div class="login_inputBlock">
-        <p class="login_inputTitle">Пароль</p>
-        <input type="text" id="city" className="login_input" />
+      <div class='login_inputBlock'>
+        <p class='login_inputTitle'>Пароль</p>
+        <input type='text' id='city' className='login_input' />
       </div>
-      <button
-        onClick={() => {
-          submit();
-        }}
-      >
-        Реєструватись
-      </button>
-      <button
-        onClick={() => {
-          setState(false);
-        }}
-      >
-        Уже існує аккаунт
-      </button>
+      <div className='buttons-content'>
+        <button
+          className='register'
+          onClick={() => {
+            submit();
+          }}
+        >
+          Реєструватись
+        </button>
+        <button
+          className='signin-button'
+          onClick={() => {
+            setState(false);
+          }}
+        >
+          Уже існує аккаунт
+        </button>
+      </div>
     </div>
   ) : (
-    <div className="login-form">
-      <div class="login_inputBlock">
-        <p class="login_inputTitle">Пошта</p>
+    <div className='login-form'>
+      <div class='login_inputBlock'>
+        <p class='login_inputTitle'>Пошта</p>
         <input
-          type="text"
+          type='text'
           value={email}
-          id="city"
-          className="login_input"
+          id='city'
+          className='login_input'
           onChange={(e) => {
             setEmail(e.target.value);
           }}
         />
       </div>
-      <div class="login_inputBlock">
-        <p class="login_inputTitle">Пароль</p>
+      <div class='login_inputBlock'>
+        <p class='login_inputTitle'>Пароль</p>
         <input
-          type="text"
+          type='text'
           value={password}
-          id="city"
-          className="login_input"
+          id='city'
+          className='login_input'
           onChange={(e) => {
             setPass(e.target.value);
           }}
         />
       </div>
-      <div className="buttonscn">
+      <div className='buttonscn'>
         <button
-          className="login-button"
+          className='login-button'
           onClick={() => {
             submit();
           }}
@@ -163,7 +167,7 @@ const Login = ({ setUser, user }) => {
           Увійти
         </button>
         <button
-          className="signin-button"
+          className='signin-button'
           onClick={() => {
             setState(true);
           }}
