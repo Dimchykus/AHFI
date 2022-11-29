f8b0740764753ac4e85b57170193f4deebb64796
remiixsp <Battlenet22>
2022-11-30 00:19:26 +0200

    main

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,37 +1,38 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import "./style.scss";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './style.scss';
 
 const Main = ({ user }) => {
   return (
-    <div className="container">
-      <h1>AHFI</h1>
-      {user.isAdmin && (
-        <NavLink className="create-vac" to="/create-vacancy">
-          Створити вакансію
+    <div className='container'>
+      <div className='all-modules'>
+        {user.isAdmin && (
+          <NavLink className='create-vac' to='/create-vacancy'>
+            Створити вакансію
+          </NavLink>
+        )}
+        <NavLink className='create-vac' to='/in-progress'>
+          Пропозиції житла
         </NavLink>
-      )}
-      <NavLink className="create-vac" to="/in-progress">
-        Пропозиції житла
-      </NavLink>
-      <NavLink className="create-vac" to="/in-progress">
-        Медичні та освітні заклади
-      </NavLink>
-      <NavLink className="create-vac" to="/in-progress">
-        Розподілення фін. допомоги організацій
-      </NavLink>
-      <NavLink className="create-vac" to="/in-progress">
-        Перепрофілювання та працевлаштування
-      </NavLink>
-      <NavLink className="create-vac" to="/in-progress">
-        Надання психологічних послуг
-      </NavLink>
-      <NavLink className="create-vac" to="/in-progress">
-        Державні виплати
-      </NavLink>
-      <NavLink className="create-vac" to="/in-progress">
-        Формування речової допомоги
-      </NavLink>
+        <NavLink className='create-vac' to='/in-progress'>
+          Медичні та освітні заклади
+        </NavLink>
+        <NavLink className='create-vac' to='/in-progress'>
+          Розподілення фін. допомоги організацій
+        </NavLink>
+        <NavLink className='create-vac' to='/in-progress'>
+          Перепрофілювання та працевлаштування
+        </NavLink>
+        <NavLink className='create-vac' to='/in-progress'>
+          Надання психологічних послуг
+        </NavLink>
+        <NavLink className='create-vac' to='/in-progress'>
+          Державні виплати
+        </NavLink>
+        <NavLink className='create-vac' to='/in-progress'>
+          Формування речової допомоги
+        </NavLink>
+      </div>
     </div>
   );
 };
